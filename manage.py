bfa266a81d5606878557fabb73bfd0f9bf421eb2
EphraimMwanda <ephraim.mwanda@gmail.com>
2021-05-28 13:05:28 +0200

    corrected manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servicesapi
-.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servicesapi.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
